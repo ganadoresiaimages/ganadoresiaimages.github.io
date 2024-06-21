284ebc7f411d827ba001793838c0c563b39fa9a8
ganadoresiaimages <judebelli545@gmail.com>
2024-06-21 14:12:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,11 +9,4 @@ function searchWinners() {
     const li = ul.getElementsByTagName('li');
 
     for (let i = 0; i < li.length; i++) {
-        let a = li[i].getElementsByTagName("a")[0];
-        if (a.innerHTML.toLowerCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
-    }
-}
+        let a =
