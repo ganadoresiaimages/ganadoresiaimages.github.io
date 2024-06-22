e9adf5d3edf8a0c5094039c88cf54c1643853f42
ganadoresiaimages <judebelli545@gmail.com>
2024-06-22 11:30:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("¡Bienvenido a Tu Sitio Web!");
+document.addEventListener('DOMContentLoaded', function () {
+    var backToTopButton = document.getElementById('backToTopButton');
+
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 300) {
+            backToTopButton.style.display = 'block';
+        } else {
+            backToTopButton.style.display = 'none';
+        }
+    });
+
+    backToTopButton.addEventListener('click', function () {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+
+    var peliculaLinks = document.querySelectorAll('.pelicula-link');
+    peliculaLinks.forEach(function (link) {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+            var imageTarget = this.getAttribute('data-image-target');
+            document.getElementById(imageTarget).scrollIntoView({ behavior: 'smooth' });
+        });
+    });
 });
-
-function searchWinners() {
-    const input = document.getElementById('searchInput');
-    const filter = input.value.toLowerCase();
-    const ul = document.getElementById('winnerList');
-    const li = ul.getElementsByTagName('li');
-
-    for (let i = 0; i < li.length; i++) {
-        let a =
